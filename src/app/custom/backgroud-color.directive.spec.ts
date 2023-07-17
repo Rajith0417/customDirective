994fcbb6dc4d5db90ebf042be9ffe3af071c0610
Raji G <rajith0417@gmail.com>
2023-07-17 19:23:21 +0530

    updates

--- src/app/custom/backgroud-color.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { BackgroudColorDirective } from './backgroud-color.directive';
-
-describe('BackgroudColorDirective', () => {
-  it('should create an instance', () => {
-    const directive = new BackgroudColorDirective();
-    expect(directive).toBeTruthy();
-  });
-});
